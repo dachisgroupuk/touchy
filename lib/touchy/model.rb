350cca8ef8422289e8847ed4ead7447faa9598a2
Alessandro Morandi <alessandro@dachisgroup.com>
2012-04-24 16:10:43 +0100

    Added safety check to ensure updating :last_active_at does not trigger an endless loop when acts_as_touchy is specified for the User model.

--- lib/touchy/model.rb
+++ lib/touchy/model.rb
@@ -17,12 +17,14 @@ module Touchy
     
     module InstanceMethods
       def be_touchy
-        current_time = current_time_from_proper_timezone
-        current_user = Touchy.current_user
-        if current_user
-          current_user.class.record_timestamps = false
-          current_user.update_attribute(:last_active_at, current_time)
-          current_user.class.record_timestamps = true
+        unless changed_attributes.has_key?('last_active_at') || !changed?
+          current_time = current_time_from_proper_timezone
+          current_user = Touchy.current_user
+          if current_user
+            current_user.class.record_timestamps = false
+            current_user.update_attribute(:last_active_at, current_time)
+            current_user.class.record_timestamps = true
+          end
         end
       end
     end
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -35,6 +35,39 @@ describe Touchy::Model do
           Timecop.freeze(time){ blog_post.be_touchy }
         }.not_to change{ user.created_at }
       end
+      
+      context "when called on the User model" do
+        class User < ActiveRecord::Base; acts_as_touchy; end
+        let(:user) { create(:user) }
+        
+        context "when nothing has changed" do
+          it "should do nothing" do
+            expect{
+              Timecop.freeze(time){ user.be_touchy }
+            }.not_to change{ user.last_active_at }
+          end
+        end
+        
+        context "when last_active_at has changed" do
+          before{ user.last_active_at = Time.now + 60 }
+          
+          it "should do nothing" do
+            expect{
+              Timecop.freeze(time){ user.be_touchy }
+            }.not_to change{ user.last_active_at }
+          end
+        end
+        
+        context "when an attribute other than last_active_at has changed" do
+          before{ user.username = 'foo' }
+          
+          it "should set last_active_at for the user" do
+            expect{
+              Timecop.freeze(time){ user.be_touchy }
+            }.to change{ user.last_active_at }.to(time)
+          end
+        end
+      end
     end
   end
 end
